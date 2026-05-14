69461140bd6153e2221d49a6204c8458f859cd02
notyash <yashdahapute7@gmail.com>
2026-05-14 23:03:03 +0000

    feat: add password hashing helper

--- rocket-backend/src/utils/auth_utils.rs
+++ rocket-backend/src/utils/auth_utils.rs
@@ -1,7 +1,7 @@
 use jsonwebtoken::{DecodingKey, Validation, decode};
 use rocket::http::{Cookie, SameSite};
-use crate::{dto::auth::AppClaims , errors::AppError };
-
+use crate::{dto::auth::AppClaims , errors::{AppError, AuthErrors} };
+use argon2::{password_hash::{rand_core::OsRng, PasswordHasher, SaltString}, Argon2};
 
 pub fn generate_jwt(sub: i32, secret_key: &str, session_duration: i64) -> Result<String, AppError> {
     let claims = AppClaims {
@@ -23,6 +23,16 @@ pub fn decode_jwt(token: &str, secret: &str) -> Result<AppClaims, AppError> {
 
     let token_data = decode::<AppClaims>(token, &decoding_key, &validation)?;
     Ok(token_data.claims)
+}
+
+pub fn hash_password(password: &str) -> Result<String, AppError> {
+    let salt = SaltString::generate(&mut OsRng);
+    let password_hash = Argon2::default()
+        .hash_password(password.as_bytes(), &salt)
+        .map_err(|_| AuthErrors::InvalidCredentials)?
+        .to_string();
+
+    Ok(password_hash)
 }
 
 pub fn build_auth_cookie(token: String, session_duration: i64) -> Cookie<'static> {
