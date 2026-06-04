7affb72d91985842e8d0730d908ce2099663624f
notyash <yashdahapute7@gmail.com>
2026-06-04 14:36:55 +0000

    feat: add handler to decrement product from cart

--- rocket-backend/src/routes/cart.rs
+++ rocket-backend/src/routes/cart.rs
@@ -1,13 +1,13 @@
 use rocket::{State, http::Status, serde::json::Json};
-use crate::{AppState, dto::{auth::AuthenticatedUser, cart::{AllProductsInCart, ItemToAddOrRemove}}, errors::AppError, 
-            repos::cart::{get_all_products_in_cart, get_existing_cart, get_or_create_cart, get_product_price, remove_product, upsert_new_product}};
+use crate::{AppState, dto::{auth::AuthenticatedUser, cart::{AllProductsInCart, ItemToAdd, ItemToRemove}}, errors::AppError, 
+            repos::cart::{get_all_products_in_cart, get_existing_cart, get_or_create_cart, get_product_price, remove_one_product_quantity, remove_product, upsert_new_product}};
 
 pub fn routes() -> Vec<rocket::Route> {
-    routes![add_to_cart, remove_from_cart, items_in_cart]
+    routes![add_product_to_cart, remove_product_from_cart, get_all_items_in_cart, decrement_product_in_cart]
 }
 
 #[get("/items")]
-async fn items_in_cart(user: AuthenticatedUser, state: &State<AppState>) -> Result<Json<Vec<AllProductsInCart>>, AppError> {
+async fn get_all_items_in_cart(user: AuthenticatedUser, state: &State<AppState>) -> Result<Json<Vec<AllProductsInCart>>, AppError> {
     let user_id = user.id;
     let cart = get_or_create_cart(user_id, state.inner()).await?;
     let products_in_cart = get_all_products_in_cart(cart.id, state).await?;
@@ -15,7 +15,7 @@ async fn items_in_cart(user: AuthenticatedUser, state: &State<AppState>) -> Resu
 }
 
 #[post("/add", data="<item>")]
-async fn add_to_cart(user: AuthenticatedUser, item: Json<ItemToAddOrRemove>, state: &State<AppState>) -> Result<Status, AppError> {
+async fn add_product_to_cart(user: AuthenticatedUser, item: Json<ItemToAdd>, state: &State<AppState>) -> Result<Status, AppError> {
     let user_id = user.id;
     let cart = get_or_create_cart(user_id, state.inner()).await?;
     let current_price = get_product_price(item.product_id, state.inner()).await?;
@@ -25,9 +25,18 @@ async fn add_to_cart(user: AuthenticatedUser, item: Json<ItemToAddOrRemove>, sta
 }
 
 #[post("/remove", data="<item>")]
-async fn remove_from_cart(user: AuthenticatedUser, item: Json<ItemToAddOrRemove>, state: &State<AppState>) -> Result<Status, AppError> {
+async fn remove_product_from_cart(user: AuthenticatedUser, item: Json<ItemToRemove>, state: &State<AppState>) -> Result<Status, AppError> {
     let user_id = user.id;
     let cart = get_existing_cart(user_id, state.inner()).await?;
     remove_product(item.product_id, cart.id, state.inner()).await?;
     Ok(Status::NoContent)
 }
+
+#[post("/decrement", data="<item>")]
+async fn decrement_product_in_cart(user: AuthenticatedUser, item: Json<ItemToRemove>, state: &State<AppState>) -> Result<Status, AppError> {
+    let user_id = user.id;
+    let cart = get_existing_cart(user_id, state.inner()).await?;
+    remove_one_product_quantity(item.product_id, cart.id, state.inner()).await?;
+    Ok(Status::NoContent)
+}
+
